9d8f8d4226f7169c3b1f8ca24ec7cf725f620604
JaviAibar <es.javi.es@gmail.com>
2024-06-13 12:09:17 +0200

    Created Pytest suit

---
+++ test_exercise341.py
@@ -0,0 +1,23 @@
+import pytest
+from Exercise341 import process_checkout_system
+
+def test_exercise341():
+    # Datos de prueba
+    data = [
+        2, 2, 
+        10, 5,
+        2, 2,
+        5, 10,
+        3, 2,
+        5, 10
+    ]
+    
+    # Resultado esperado para los datos de prueba
+    expected_result = [3, 1, 2]
+    
+    # Llamada a la función y verificación del resultado
+    result = process_checkout_system(data)
+    assert result == expected_result
+
+if __name__ == "__main__":
+    pytest.main()
