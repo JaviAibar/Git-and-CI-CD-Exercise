1b0493d6f0754fe06fa05908bc721314584e20b4
JaviAibar <es.javi.es@gmail.com>
2024-06-13 12:23:00 +0200

    Changes

--- exercise341.py
+++ exercise341.py
@@ -1,8 +1,6 @@
-import sys
-
-def clean_buffer(clients_len):
-    for _ in range(clients_len):
-        next(sys.stdin)
+def clean_buffer(data, idx, clients_len):
+    idx += clients_len
+    return idx
 
 def reduce_by_min(array):
     min_value = obtener_numero_mas_bajo(array)
@@ -18,10 +16,9 @@ def reduce_by_min(array):
 def obtener_numero_mas_bajo(lista_enteros):
     return min(lista_enteros)
 
-def main():
-    with open('input.txt', 'r') as file:
-        data = file.read().split()
+def process_checkout_system(data):
     idx = 0
+    results = []
     
     while idx < len(data):
         checkout_len = int(data[idx])
@@ -30,13 +27,13 @@ def main():
         idx += 1
 
         if checkout_len > clients_len:
-            idx += clients_len
-            print(clients_len + 1)
+            idx = clean_buffer(data, idx, clients_len)
+            results.append(clients_len + 1)
             continue
 
         if checkout_len == 1:
-            idx += clients_len
-            print(1)
+            idx = clean_buffer(data, idx, clients_len)
+            results.append(1)
             continue
 
         checkouts = [0] * checkout_len
@@ -57,11 +54,10 @@ def main():
             empties = reduce_by_min(checkouts)
             for e in empties:
                 if client_index >= clients_len:
-                    print(e + 1)
+                    results.append(e + 1)
                     found = True
                     break
                 checkouts[e] = clients[client_index]
                 client_index += 1
 
-if __name__ == "__main__":
-    main()
+    return results
